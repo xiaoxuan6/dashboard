4a91aded8e8f33f61e4cf83fe507bfd0eb849e89
xiaoxuan6 <1527736751@qq.com>
2023-09-13 22:15:56 +0800

    feat: Add config.json

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 [![img](./static/img.png)](https://xiaoxuan6.vercel.app/)
 
+[vercel.json](https://vercel.com/docs/projects/project-configuration)
+
 # Demo
 
 [blog](https://github.com/miaogaolin/printlove)
--- lib/index_handler.go
+++ lib/index_handler.go
@@ -1,12 +1,25 @@
 package lib
 
 import (
-    "dashboard/constants"
+    "dashboard/config"
+    _ "embed"
+    "encoding/json"
+    "errors"
+    "fmt"
 )
+
+//go:embed config.json
+var settings []byte
 
 type IndexHandler struct {
 }
 
 func (i IndexHandler) Run() *Response {
-    return successWithData(constants.Settings)
+    var c config.Config
+    err := json.Unmarshal(settings, &c)
+    if err != nil {
+        return fail(errors.New(fmt.Sprintf("json 解析错误：%s", err.Error())))
+    }
+
+    return successWithData(c)
 }
