d89903ea5549a1b2a1e61fc0f6677e266aae22bc
xiaoxuan6 <1527736751@qq.com>
2023-09-14 20:51:40 +0800

    fix: Update search.js

--- static/js/search.js
+++ static/js/search.js
@@ -1,15 +1,21 @@
-function search() {
+init()
+
+function init() {
     let token = localStorage.getItem("token")
     let email = localStorage.getItem("email")
-    let passwd = localStorage.getItem("password")
-    if (!token || !email || !passwd) {
+    console.log("token", token)
+    console.log("email", email)
+    
+    if (!token || !email) {
         Notiflix.Notify.failure("未登录！");
         setTimeout(function () {
             window.location.href = "/login"
-        }, 1000)
+        }, 2000)
         return
     }
+}
 
+function search() {
     let keyword = document.getElementById("keyword").value
     if (!keyword) {
         error("请输入搜索问题")
