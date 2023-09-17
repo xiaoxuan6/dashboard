ea2a639ef8e69d80772ce6ef111822442a6d01f8
xiaoxuan6 <1527736751@qq.com>
2023-09-17 14:36:30 +0800

    fix: Update github

--- lib/github_handle.go
+++ lib/github_handle.go
@@ -1,6 +1,9 @@
 package lib
 
 import (
+    "encoding/json"
+    "fmt"
+    "io/ioutil"
     "net/http"
 )
 
@@ -16,18 +19,37 @@ type TokenRequest struct {
 }
 
 func (g GithubHandler) Do(r *http.Request) *Response {
-    //_ = r.ParseForm()
-    //token := r.FormValue("token")
+    res := struct {
+        Token  string `json:"token"`
+        Token1 string `json:"token_1"`
+        Token2 string `json:"token_2"`
+        Token3 string `json:"token_3"`
+    }{}
 
-    token := r.URL.Query().Get("token")
-    return SuccessWithData(token)
+    _ = r.ParseForm()
+    res.Token = r.FormValue("token")
+
+    res.Token1 = r.URL.Query().Get("token")
+
+    b, _ := ioutil.ReadAll(r.Body)
+    res.Token2 = string(b)
+
+    var request TokenRequest
+    err := json.Unmarshal(b, &request)
+    if err != nil {
+        res.Token3 = fmt.Sprintf("json 解析请求惨错误：%s", err)
+    } else {
+        res.Token3 = request.Token
+    }
+
+    return SuccessWithData(res)
 
     //expirationAt := os.Getenv("CACHE_TOKEN_EXPIRATION_AT")
     //i, _ := strconv.Atoi(expirationAt)
     //d := time.Duration(i) * 7
     //cache.Cache.Set(common.Token, token, d)
 
-    return SuccessWithData(token)
+    //return SuccessWithData(token)
 
     LoadData()
 
