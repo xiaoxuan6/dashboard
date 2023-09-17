d9474e904e9c919e21838e96d8b038369c1bc782
xiaoxuan6 <1527736751@qq.com>
2023-09-17 14:48:06 +0800

    fix: Update github

--- lib/github_handle.go
+++ lib/github_handle.go
@@ -1,10 +1,15 @@
 package lib
 
 import (
+    "dashboard/common"
+    "dashboard/pkg/cache"
     "encoding/json"
     "fmt"
     "io/ioutil"
     "net/http"
+    "os"
+    "strconv"
+    "time"
 )
 
 type GithubHandler struct {
@@ -19,41 +24,22 @@ type TokenRequest struct {
 }
 
 func (g GithubHandler) Do(r *http.Request) *Response {
-
-    _ = r.ParseForm()
-    token := r.FormValue("token")
-
     b, _ := ioutil.ReadAll(r.Body)
 
     var request TokenRequest
-    var token3 string
     err := json.Unmarshal(b, &request)
     if err != nil {
-        token3 = fmt.Sprintf("json 解析请求惨错误：%s", err)
-    } else {
-        token3 = request.Token
+        return FailWithMsg(fmt.Sprintf("json 解析请求惨错误：%s", err))
     }
 
-    res := struct {
-        Token  string `json:"token"`
-        Token1 string `json:"token_1"`
-        Token2 string `json:"token_2"`
-        Token3 string `json:"token_3"`
-    }{
-        Token:  token,
-        Token1: r.URL.Query().Get("token"),
-        Token2: string(b),
-        Token3: token3,
+    if len(request.Token) < 1 {
+        return FailWithMsg("github token 不能为空！")
     }
 
-    return SuccessWithData(&res)
-
-    //expirationAt := os.Getenv("CACHE_TOKEN_EXPIRATION_AT")
-    //i, _ := strconv.Atoi(expirationAt)
-    //d := time.Duration(i) * 7
-    //cache.Cache.Set(common.Token, token, d)
-
-    //return SuccessWithData(token)
+    expirationAt := os.Getenv("CACHE_TOKEN_EXPIRATION_AT")
+    i, _ := strconv.Atoi(expirationAt)
+    d := time.Duration(i) * 7
+    cache.Cache.Set(common.Token, request.Token, d)
 
     LoadData()
 
