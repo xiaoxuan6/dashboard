baf6e27cfa7c3e0c2ee476503bbe6d0669c83ec7
xiaoxuan6 <1527736751@qq.com>
2023-09-17 14:43:31 +0800

    fix: Update github

--- lib/github_handle.go
+++ lib/github_handle.go
@@ -19,30 +19,34 @@ type TokenRequest struct {
 }
 
 func (g GithubHandler) Do(r *http.Request) *Response {
+
+    _ = r.ParseForm()
+    token := r.FormValue("token")
+
+    b, _ := ioutil.ReadAll(r.Body)
+
+    var request TokenRequest
+    var token3 string
+    err := json.Unmarshal(b, &request)
+    if err != nil {
+        token3 = fmt.Sprintf("json 解析请求惨错误：%s", err)
+    } else {
+        token3 = request.Token
+    }
+
     res := struct {
         Token  string `json:"token"`
         Token1 string `json:"token_1"`
         Token2 string `json:"token_2"`
         Token3 string `json:"token_3"`
-    }{}
-
-    _ = r.ParseForm()
-    res.Token = r.FormValue("token")
-
-    res.Token1 = r.URL.Query().Get("token")
-
-    b, _ := ioutil.ReadAll(r.Body)
-    res.Token2 = string(b)
-
-    var request TokenRequest
-    err := json.Unmarshal(b, &request)
-    if err != nil {
-        res.Token3 = fmt.Sprintf("json 解析请求惨错误：%s", err)
-    } else {
-        res.Token3 = request.Token
+    }{
+        Token:  token,
+        Token1: r.URL.Query().Get("token"),
+        Token2: string(b),
+        Token3: token3,
     }
 
-    return SuccessWithData(res)
+    return SuccessWithData(&res)
 
     //expirationAt := os.Getenv("CACHE_TOKEN_EXPIRATION_AT")
     //i, _ := strconv.Atoi(expirationAt)
