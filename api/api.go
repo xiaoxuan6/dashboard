2236b387fbe040a60732d006d7f965200b1e66fc
xiaoxuan6 <1527736751@qq.com>
2023-09-19 20:10:23 +0800

    feat: Add apis

---
+++ api/api.go
@@ -0,0 +1,28 @@
+package api
+
+import (
+    "dashboard/routers"
+    "fmt"
+    "github.com/gin-gonic/gin"
+    "net/http"
+)
+
+var app *gin.Engine
+
+func init() {
+    app = gin.New()
+
+    app.NoRoute(func(c *gin.Context) {
+        path := c.Request.URL.Path
+        c.JSON(http.StatusBadRequest, gin.H{
+            "status": http.StatusNotFound,
+            "msg":    fmt.Sprintf("%s not found", path),
+        })
+    })
+
+    routers.RegisterRouter(app)
+}
+
+func Api(w http.ResponseWriter, r *http.Request) {
+    app.ServeHTTP(w, r)
+}
---
+++ routers/router.go
@@ -0,0 +1,15 @@
+package routers
+
+import (
+    "github.com/gin-gonic/gin"
+    "net/http"
+)
+
+func RegisterRouter(r *gin.Engine) {
+    r.GET("/", func(context *gin.Context) {
+        context.JSON(http.StatusOK, gin.H{
+            "status": 200,
+            "msg":    "ok",
+        })
+    })
+}
